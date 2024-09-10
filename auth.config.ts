737e490c2dc1899e827795fdb82edee3a34aae7f
Misan <0xmisan@gmail.com>
2024-09-10 15:28:32 +0800

    新增github登录

--- auth.config.ts
+++ auth.config.ts
@@ -1,9 +1,12 @@
 import type { NextAuthConfig } from "next-auth";
+import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import Resend from "next-auth/providers/resend";
 
 import { env } from "@/env.mjs";
 import { sendVerificationRequest } from "@/lib/email";
+
+// https://authjs.dev/guides/configuring-github
 
 export default {
   providers: [
@@ -11,6 +14,7 @@ export default {
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
+    GitHub,
     Resend({
       apiKey: env.RESEND_API_KEY,
       from: env.EMAIL_FROM,
